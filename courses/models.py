da3fabafba6b4911b937fc9b50d034d013a1e61a
arjunkrishna_edulyft <arjun@edulyft.com>
2015-03-06 22:19:27 +0530

    first commit

---
+++ courses/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Subject(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class Course(models.Model):
+    subject = models.ForeignKey(Subject, related_name='courses')
+    name = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class CourseChapter(models.Model):
+    name = models.CharField(max_length=40)
+    course = models.ForeignKey(Course, related_name='chapters')
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class CourseConcept(models.Model):
+    name = models.CharField(max_length=40)
+    chapter = models.ForeignKey(CourseChapter, related_name='concepts')
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
