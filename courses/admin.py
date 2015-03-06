c535da75e756e83f95a37afb9ec4873a2c342dc4
arjunkrishna_edulyft <arjun@edulyft.com>
2015-03-07 00:33:15 +0530

    Added Concept Content model and the admin integration for courses using the ckeditor app for django

--- courses/admin.py
+++ courses/admin.py
@@ -1,1 +1,50 @@
-__author__ = 'root'
+from django.contrib import admin
+import models
+#from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+class ContentConceptInline(admin.StackedInline):
+    model = models.CourseConceptContent
+    max_num = 1
+
+class ConceptChapterInline(admin.StackedInline):
+    model = models.CourseConcept
+    max_num = 4
+
+class ChapterCourseInline(admin.StackedInline):
+    model = models.CourseChapter
+    max_num = 4
+
+class CoursesSubjectInline(admin.StackedInline):
+    model = models.Course
+    max_num = 1
+
+class CourseModelAdmin(admin.ModelAdmin):
+    inlines = [ChapterCourseInline]
+    search_fields = ['name','details']
+    list_filter = ['subject']
+    list_display = ['name', 'subject_name']
+
+class SubjectModelAdmin(admin.ModelAdmin):
+    inlines = [CoursesSubjectInline]
+    search_fields = ['name']
+
+
+class ChapterModelAdmin(admin.ModelAdmin):
+    inlines = [ConceptChapterInline]
+    list_filter = ['course_name']
+
+class ConceptModelAdmin(admin.ModelAdmin):
+    list_filter = ['chapter_name', 'course_name']
+    list_display = ['name', 'chapter_name', 'course_name']
+
+class ConceptContentModelAdmin(admin.ModelAdmin):
+    list_filter = ['course_name']
+    search_fields = ['content', 'chapter_name', 'course_name', 'concept_name']
+    list_display = ['__unicode__', 'chapter_name', 'course_name']
+
+
+admin.site.register(models.Subject, SubjectModelAdmin)
+admin.site.register(models.Course, CourseModelAdmin)
+admin.site.register(models.CourseChapter, CourseModelAdmin)
+admin.site.register(models.CourseConcept, ConceptModelAdmin)
+admin.site.register(models.CourseConceptContent, ConceptContentModelAdmin)
+
--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Subject(models.Model):
     name = models.CharField(max_length=40)
@@ -7,30 +8,85 @@ class Subject(models.Model):
     def __unicode__(self):
         return unicode(self.name)
 
+    def get_name(self):
+        return self.name
+
 
 class Course(models.Model):
     subject = models.ForeignKey(Subject, related_name='courses')
     name = models.CharField(max_length=40)
+    details = RichTextField()
 
     def __unicode__(self):
         return unicode(self.name)
+
+    def get_name(self):
+        return self.name
+
+    @property
+    def subject_name(self):
+        return unicode(self.subject.get_name())
 
 
 class CourseChapter(models.Model):
+    class Meta:
+        verbose_name_plural = "Course's Chapters"
     name = models.CharField(max_length=40)
     course = models.ForeignKey(Course, related_name='chapters')
+    details = RichTextField()
 
     def __unicode__(self):
         return unicode(self.name)
+
+    @property
+    def course_name(self):
+        return unicode(self.course.get_name())
+
+    def get_name(self):
+        return self.name
 
 
 class CourseConcept(models.Model):
+    class Meta:
+        verbose_name_plural = "Chapter's Concepts"
     name = models.CharField(max_length=40)
     chapter = models.ForeignKey(CourseChapter, related_name='concepts')
-    weightage = models.IntegerField(default=0.5)
+    weightage = models.IntegerField(default=0.5, help_text="This is used while calculating the percentage"
+                                                           " completion of the course for a user")
 
     def __unicode__(self):
         return unicode(self.name)
+
+    @property
+    def chapter_name(self):
+        return unicode(self.chapter.name)
+
+    @property
+    def course_name(self):
+        return self.chapter.course_name()
+
+    def get_name(self):
+        return self.name
+
+class CourseConceptContent(models.Model):
+    course_concept = models.OneToOneField(CourseConcept, related_name='content')
+    content = RichTextField()
+
+    def __unicode__(self):
+        return self.course_concept.name + "\'s" + "Content"
+
+    @property
+    def concept_name(self):
+        return unicode(self.course_concept.get_name())
+
+    @property
+    def chapter_name(self):
+        return unicode(self.course_concept.chapter_name())
+
+    @property
+    def course_name(self):
+        return unicode(self.course_concept.course_name())
+
 
 
 
